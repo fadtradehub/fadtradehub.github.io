db9a40f62e5266906a8e0e9189c205457cd118c7
fadtradehub <fadtradehub@gmail.com>
2026-01-18 14:49:35 +0700

    first commit

---
+++ react/contactform.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function ContactForm() {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    formData.append("access_key", "60628023-d232-4e9f-b2e0-a3d9cb7ac5c3");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+    setResult(data.success ? "Success!" : "Error");
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" name="name" required/>
+      <input type="email" name="email" required/>
+      <textarea name="message" required></textarea>
+      <button type="submit">Submit</button>
+      <p>{result}</p>
+    </form>
+  );
+}
